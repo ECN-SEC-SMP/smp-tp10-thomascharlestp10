9bee1f4447e79842e1e347e8819494ae6740ea68
CharlesSWE <charles.mabit92@gmail.com>
2026-03-16 11:56:36 +0100

    maj

--- build/_deps/googletest-subbuild/googletest-populate-prefix/tmp/googletest-populate-gitclone.cmake
+++ build/_deps/googletest-subbuild/googletest-populate-prefix/tmp/googletest-populate-gitclone.cmake
@@ -3,11 +3,11 @@
 
 cmake_minimum_required(VERSION 3.5)
 
-if(EXISTS "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps" AND EXISTS "googletest-src" AND
-  "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps" IS_NEWER_THAN "googletest-src")
+if(EXISTS "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/googletest-populate-gitclone-lastrun.txt" AND EXISTS "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/googletest-populate-gitinfo.txt" AND
+  "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/googletest-populate-gitclone-lastrun.txt" IS_NEWER_THAN "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/googletest-populate-gitinfo.txt")
   message(STATUS
     "Avoiding repeated git clone, stamp file is up to date: "
-    "'/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps'"
+    "'/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/googletest-populate-gitclone-lastrun.txt'"
   )
   return()
 endif()
@@ -26,8 +26,8 @@ set(number_of_tries 0)
 while(error_code AND number_of_tries LESS 3)
   execute_process(
     COMMAND "/usr/bin/git"
-            clone --no-checkout --depth 1 --no-single-branch --origin "DOWNLOAD_EXTRACT8TIMESTAMP" -c http.sslVerify=true "https://github.com/google/googletest.git" ""
-    WORKING_DIRECTORY "advice.detachedHead=false"
+            clone --no-checkout --config "advice.detachedHead=false" "https://github.com/google/googletest.git" "googletest-src"
+    WORKING_DIRECTORY "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps"
     RESULT_VARIABLE error_code
   )
   math(EXPR number_of_tries "${number_of_tries} + 1")
@@ -42,7 +42,7 @@ endif()
 execute_process(
   COMMAND "/usr/bin/git"
           checkout "v1.17.0" --
-  WORKING_DIRECTORY "advice.detachedHead=false/"
+  WORKING_DIRECTORY "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-src"
   RESULT_VARIABLE error_code
 )
 if(error_code)
@@ -52,22 +52,22 @@ endif()
 set(init_submodules TRUE)
 if(init_submodules)
   execute_process(
-    COMMAND "/usr/bin/git" -c;http.sslVerify=true
-            submodule update origin --init TRUE
-    WORKING_DIRECTORY "advice.detachedHead=false/"
+    COMMAND "/usr/bin/git" 
+            submodule update --recursive --init 
+    WORKING_DIRECTORY "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-src"
     RESULT_VARIABLE error_code
   )
 endif()
 if(error_code)
-  message(FATAL_ERROR "Failed to update submodules in: 'advice.detachedHead=false/'")
+  message(FATAL_ERROR "Failed to update submodules in: '/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-src'")
 endif()
 
 # Complete success, update the script-last-run stamp file:
 #
 execute_process(
-  COMMAND ${CMAKE_COMMAND} -E copy "googletest-src" "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps"
+  COMMAND ${CMAKE_COMMAND} -E copy "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/googletest-populate-gitinfo.txt" "/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/googletest-populate-gitclone-lastrun.txt"
   RESULT_VARIABLE error_code
 )
 if(error_code)
-  message(FATAL_ERROR "Failed to copy script-last-run stamp file: '/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps'")
+  message(FATAL_ERROR "Failed to copy script-last-run stamp file: '/home/charl/Documents/SMP/TP10/smp-tp10-thomascharlestp10/build/_deps/googletest-subbuild/googletest-populate-prefix/src/googletest-populate-stamp/googletest-populate-gitclone-lastrun.txt'")
 endif()
