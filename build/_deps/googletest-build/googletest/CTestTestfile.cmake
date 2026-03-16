8f77f7793a2b0bfbc4397a185033fc690a4ddd66
Astraz2 <thomas.david2603@gmail.com>
2026-03-16 10:54:13 +0100

    Stop tracking nom_du_dossier

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thomas/Documents/SMP/smp-tp10-thomascharlestp10/build/_deps/googletest-src/googletest
-# Build directory: /home/thomas/Documents/SMP/smp-tp10-thomascharlestp10/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
